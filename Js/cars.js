f325bf0ee0b2f28c598bb70ddf7032ee291c446a
MartL7 <latiapelitos123@gmail.com>
2024-06-24 00:11:46 -0600

    Cars-Shop

---
+++ Js/cars.js
@@ -0,0 +1,142 @@
+export const cars = [
+  {
+    id: 'Car-1',
+    name: 'Ferrarri 1',
+    image: './img/ferrari-1.webp',
+    price: 125000,
+    category: 'Ferrari'
+  },
+  {
+    id: 'Car-2',
+    name: 'Ferrarri 2',
+    image: './img/ferrari-2.webp',
+    price: 270000,
+    category: 'Ferrari'
+  },
+  {
+    id: 'Car-3',
+    name: 'Ferrarri 3',
+    image: './img/ferrari-3.webp',
+    price: 315000,
+    category: 'Ferrari'
+  },
+  {
+    id: 'Car-4',
+    name: 'Ferrarri 4',
+    image: './img/ferrari-4.webp',
+    price: 5000000,
+    category: 'Ferrari'
+  },
+  {
+    id: 'Car-5',
+    name: 'Ferrarri 5',
+    image: './img/ferrari-5.webp',
+    price: 150000,
+    category: 'Ferrari'
+  },
+  {
+    id: 'Car-6',
+    name: 'Lamborghini 1',
+    image: './img/lambo-1.webp',
+    price: 500000,
+    category: 'Lamborghini'
+  },
+  {
+    id: 'Car-7',
+    name: 'Lamborghini 2',
+    image: './img/lambo-2.webp',
+    price: 475000,
+    category: 'Lamborghini'
+  },
+  {
+    id: 'Car-8',
+    name: 'Lamborghini 3',
+    image: './img/lambo-3.webp',
+    price: 600000,
+    category: 'Lamborghini'
+  },
+  {
+    id: 'Car-9',
+    name: 'Lamborghini 4',
+    image: './img/lambo-4.webp',
+    price: 750000,
+    category: 'Lamborghini'
+  },
+  {
+    id: 'Car-10',
+    name: 'Lamborghini 5',
+    image: './img/lambo-5.webp',
+    price: 800000,
+    category: 'Lamborghini'
+  },
+  {
+    id: 'Car-11',
+    name: 'Porsche 1',
+    image: './img/porsche-1.webp',
+    price: 200000,
+    category: 'Porsche'
+  },
+  {
+    id: 'Car-12',
+    name: 'Porsche 2',
+    image: './img/porsche-2.webp',
+    price: 250000,
+    category: 'Porsche'
+  },
+  {
+    id: 'Car-13',
+    name: 'Porsche 3',
+    image: './img/porsche-3.webp',
+    price: 300000,
+    category: 'Porsche'
+  },
+  {
+    id: 'Car-14',
+    name: 'Porsche 4',
+    image: './img/porsche-4.webp',
+    price: 350000,
+    category: 'Porsche'
+  },
+  {
+    id: 'Car-15',
+    name: 'Porsche 5',
+    image: './img/porsche-5.webp',
+    price: 400000,
+    category: 'Porsche'
+  },
+  {
+    id: 'Car-16',
+    name: 'Bmw 1',
+    image: './img/bmw-1.webp',
+    price: 100000,
+    category: 'Bmw'
+  },
+  {
+    id: 'Car-17',
+    name: 'Bmw 2',
+    image: './img/bmw-2.webp',
+    price: 150000,
+    category: 'Bmw'
+  },
+  {
+    id: 'Car-18',
+    name: 'Bmw 3',
+    image: './img/bmw-3.webp',
+    price: 275000,
+    category: 'Bmw'
+  },
+  {
+    id: 'Car-19',
+    name: 'Bmw 4',
+    image: './img/bmw-4.webp',
+    price: 320000,
+    category: 'Bmw'
+  },
+  {
+    id: 'Car-20',
+    name: 'Bmw 5',
+    image: './img/bmw-5.webp',
+    price: 190000,
+    category: 'Bmw'
+  }
+]
